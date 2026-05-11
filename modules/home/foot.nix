a1f7cd0375f9e57067380cdc3367d8102450e3c1
RemiErr <re.err236@gmail.com>
2026-05-11 23:15:27 +0800

    Update style

--- modules/home/foot.nix
+++ modules/home/foot.nix
@@ -11,9 +11,9 @@
         dpi-aware        = "no";
         resize-keep-grid = "no";
       };
-      colors-dark = {
-        alpha = 0.72;
-        # blur = true;  # 需要 foot >= 1.26 + compositor，VirtualBox 暫不啟用
+      colors = {
+        alpha = "0.72";
+        blur = true;
       };
       mouse = {
         hide-when-typing = "yes";
