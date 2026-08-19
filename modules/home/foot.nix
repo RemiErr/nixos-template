cb9d22c95f7de652d67f226794debece66351f73
RemiErr <Re.Err236@gmail.com>
2026-08-19 18:06:45 +0800

    feat: update foot settings

--- modules/home/foot.nix
+++ modules/home/foot.nix
@@ -24,8 +24,20 @@
         hide-when-typing = "yes";
         alternate-scroll-mode = "yes";
       };
+      
+      # foot 鍵位對調右鍵與中鍵
+      # 另外綁 primary-paste
+      #
+      # ref> https://man.archlinux.org/man/foot.ini.5
+      #
+      mouse-bindings = {
+        primary-paste   = "Shift+BTN_RIGHT";
+        select-extend   = "BTN_MIDDLE";
+        clipboard-paste = "BTN_RIGHT";
+      };
 
-      colors = {
+      # 新版 foot 把 [colors] 拆成 [colors-dark]/[colors-light]
+      colors-dark = {
         alpha  = "0.85";
         cursor = "141311 FFB4AB";  # text-on-cursor  cursor-bg
 
